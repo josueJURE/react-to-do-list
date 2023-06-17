5bb1fd02725f76f67894a743c247f07df64888d5
josueJURE <josue.jure@gmail.com>
2023-06-17 14:57:34 +0100

    add setInput('') so that input field clears right after user clicks on add button

--- src/Input.jsx
+++ src/Input.jsx
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
 
 function Input(props) {
+  const [input, setInput] = useState("");
 
-    const [input, setInput] = useState("");
-    
-    function HandleChange(e) {
-        const newInput = e.target.value;
-        setInput(newInput)
+  function HandleChange(e) {
+    const newInput = e.target.value;
+    setInput(newInput);
+  }
 
-    }
-
-    function submitNote() {
-        props.onAdd(input)
-    }
-
-
+  function submitNote() {
+    props.onAdd(input);
+    setInput("");
+  }
 
   return (
     <>
@@ -22,7 +19,13 @@ function Input(props) {
         {/* <i className="fa" aria-hidden="true"></i>
         <i id="add" class="fa-solid fa-plus-circle"></i> */}
         <button onClick={submitNote}>add</button>
-        <input onChange={HandleChange} id="userInput" type="text" required="" value={input}></input>
+        <input
+          onChange={HandleChange}
+          id="userInput"
+          type="text"
+          required=""
+          value={input}
+        ></input>
       </div>
     </>
   );
