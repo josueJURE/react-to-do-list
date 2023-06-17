dd5780d1145616394ddb34207794cf3bafd51cd5
josueJURE <josue.jure@gmail.com>
2023-06-17 14:45:10 +0100

    pass false as a reserved keyword in useState() instead of writing it as a string. function now working as expected

--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -2,20 +2,27 @@ import React, { useState } from "react";
 import "./Tasks.css";
 
 function Tasks(props) {
+  const [isDone, setIsDone] = useState(false);
 
-  const [isDone, setIsDone] = useState("false")
+  // const [isEditable, setEditable] = useState(false);
+
+  // function handleEditable() {
+  //   setEditable(previous => !previous)
+  // }
 
   function handleChange() {
-    setIsDone(previous => !previous)
+    setIsDone((previous) => !previous);
   }
-
 
   return (
     <div id={props.id} className="row">
       <div>{props.number}</div>
       <input onChange={handleChange} type="checkbox"></input>
-      <div style={{textDecoration: isDone ? "line-through" : "none"}}>{props.task}</div>
-      <button onClick={()=> props.onDelete(props.id)}>delete</button>
+      <div style={{ textDecoration: isDone ? "line-through" : "none" }}>
+        {props.task}
+      </div>
+      <button onClick={() => props.onDelete(props.id)}>delete</button>
+      <button>Edit</button>
     </div>
   );
 }
