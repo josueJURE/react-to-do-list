4d7274d247f53e6352b5982fe95afa3e41adb225
josueJURE <josue.jure@gmail.com>
2023-06-17 11:00:19 +0100

    add functionality in Task component so that element is crossed out when when user ticks checkbox and vice versa

--- src/Tasks.jsx
+++ src/Tasks.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Tasks.css";
 
 function Tasks(props) {
+
+  const [isDone, setIsDone] = useState("false")
+
+  function handleChange() {
+    setIsDone(previous => !previous)
+  }
+
+
   return (
     <div id={props.id} className="row">
       <div>{props.number}</div>
-      <input type="checkbox"></input>
-      <div>{props.task}</div>
+      <input onChange={handleChange} type="checkbox"></input>
+      <div style={{textDecoration: isDone ? "line-through" : "none"}}>{props.task}</div>
       <button onClick={()=> props.onDelete(props.id)}>delete</button>
     </div>
   );
